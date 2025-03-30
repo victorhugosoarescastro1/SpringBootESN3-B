66f1ea2ee1b716b0d6bc5cbd33c4886d28e62c69
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-03-30 12:17:04 -0300

    My First Commit :)

---
+++ UnifanEngenhariaSoftwareESNB3/src/main/java/com/springboot/UnifanEngenhariaSoftwareESNB3/VictorHugoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.UnifanEngenhariaSoftwareESNB3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictorHugoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictorHugoSpringBootApplication.class, args);
+	}
+
+}
---
+++ UnifanEngenhariaSoftwareESNB3/src/test/java/com/springboot/UnifanEngenhariaSoftwareESNB3/VictorHugoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.UnifanEngenhariaSoftwareESNB3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorHugoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
